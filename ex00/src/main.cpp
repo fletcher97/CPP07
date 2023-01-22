bd6bac68877a3a1df3f3fb48d233ee959ce42a62
Miguel Gueifao <miguel.gueifao@gmail.com>
2023-01-22 16:15:48 +0100

    Added given tests by subject

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -3,9 +3,29 @@
 #include "whatever.hpp"
 
 
+void
+givenTests() {
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+}
+
 int
 main()
 {
+	givenTests();
+
+	std::cout << std::endl;
+
 	int i1 = 42, i2 = 10;
 	float f1 = 4.2f, f2 = -1.0f;
 	char c1 = '*', c2 = 'B';
